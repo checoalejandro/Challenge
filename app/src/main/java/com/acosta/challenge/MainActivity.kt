abcd39bd9e8398d9a28129b9a07c7a7e9db460fc
Sergio Acosta <sergio.acosta@oracle.com>
2022-05-09 21:33:18 -0500

    @ui: Initial UI implementation

--- app/src/main/java/com/acosta/challenge/MainActivity.kt
+++ app/src/main/java/com/acosta/challenge/MainActivity.kt
@@ -3,6 +3,7 @@ package com.acosta.challenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,26 +11,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.acosta.challenge.databinding.ActivityMainBinding
 import com.acosta.challenge.ui.theme.ChallengeTheme
 import com.acosta.challenge.ui.viewmodels.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: HomeViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ChallengeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
     }
 }
 
